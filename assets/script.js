16e386dbc181906736701e63802653277438a122
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-10-24 13:50:06 +0700

    add function to render and save to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 const readlist = [];
 
-const RENDER_ELEMENT = 'render-readlist';
+const RENDER_EVENT = 'render-readlist';
 
 const STORAGE_KEY = 'READ_LIST';
 
@@ -10,12 +10,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
     AddForm.addEventListener('submit', (event)=>{
         event.preventDefault();
-        
+        addReadList();
     });
-});
 
-document.addEventListener(SAVE_TO_STORAGE, () => {
-
+    if(isStorageExist()){
+        loadData();
+    }
 });
 
 const addReadList = () => {
@@ -28,8 +28,73 @@ const addReadList = () => {
 
     readlist.push(readlistObject);
 
-    document.dispatchEvent(new Event(RENDER_ELEMENT));
+    document.dispatchEvent(new Event(RENDER_EVENT));
     saveData();
+}
+
+const addToCompleted = (id) => {
+    const book = findReadlist(id);
+    
+    if(book == null) return;
+
+    book.isCompleted = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
+}
+
+const renderReadlist = (book) => {
+    const bookItem = document.createElement("article");
+    bookItem.classList.add('book_item');
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.innerText = book.title;
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.innerText = `Penulis : ${book.author}`;
+
+    const bookYear = document.createElement("p");
+    bookYear.innerText = `Tahun : ${book.year}`;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("action");
+    
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("red");
+    deleteButton.innerText = "Hapus buku";
+
+    deleteButton.addEventListener("click", () => {
+        deleteBook(book.id);
+    });
+    
+    if(book.isCompleted) {
+        const uncompleteButton = document.createElement("button");
+        uncompleteButton.classList.add("green");
+        uncompleteButton.innerText = "Belum Selesai Dibaca";
+
+        uncompleteButton.addEventListener('click', () => {
+            undoFromComplete(book.id);
+        });
+
+        buttonContainer.append(uncompleteButton);
+        
+    } else {
+        const completeButton = document.createElement("button");
+        completeButton.classList.add("green");
+        completeButton.innerText = "Selesai Dibaca";
+        
+        completeButton.addEventListener("click", () => {
+            addToCompleted(book.id);
+        });
+
+        buttonContainer.append(completeButton);
+    }
+
+    buttonContainer.append(deleteButton);
+
+    bookItem.append(bookTitle, bookAuthor, bookYear, buttonContainer);
+    bookItem.setAttribute('id', `read-${book.id}`);
+
+    return bookItem;
 }
 
 const isStorageExist = () => {
@@ -50,3 +115,25 @@ const saveData = () => {
         localStorage.setItem(STORAGE_KEY, readlistParsed);
     }
 };
+
+const findReadlist = (id) => {
+    for (const book of readlist) {
+        if(book.id === id){
+            return book;
+        }
+    }
+    return null;
+};
+
+const loadData = () => {
+    const localData = localStorage.getItem(STORAGE_KEY);
+    const data = JSON.parse(localData);
+
+    if(isStorageExist()){
+        for(const book of data){
+            readlist.push(book);
+        }
+    }
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
+}
