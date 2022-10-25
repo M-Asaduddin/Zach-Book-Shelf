34edb885090f7fbf37acc6e40dc43825f04786a5
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-10-25 13:49:37 +0700

    add overlay when search

--- assets/script.js
+++ assets/script.js
@@ -11,10 +11,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const searchForm = document.getElementById('searchBook');
     const completeCheckBox = document.getElementById('inputBookIsComplete');
     const spanText = document.querySelector('.input_section > form > button > span');
+    const result = document.getElementById('search_result');
+    const bookList = document.querySelector("#search_result > .book_shelf > .book_list");
     
     searchForm.style.display = 'none';
+    result.style.display = 'none';
 
     AddForm.addEventListener('submit', ()=>{
+
         addReadList();
     });
 
@@ -22,18 +26,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
     searchTrigger.addEventListener('click', ()=>{
         const icon = document.querySelector('#magnifier>path');
         if(searchForm.style.display === 'none'){
+            document.body.style.overflowY = 'hidden';
             searchForm.style.display = 'flex';
+            result.style.display = 'flex';
             icon.setAttribute('d', 'M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z');
         } else {
+            document.body.style.overflowY = 'scroll';
             searchForm.style.display = 'none';
+            result.style.display = 'none';
             icon.setAttribute('d', 'M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z');
+            bookList.innerHTML = "";
         }
 
     });
 
-    searchForm.addEventListener('submit', ()=>{
-        searchTitle();
-        searchTrigger.dispatchEvent(new Event('click'));
+    searchForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        bookList.innerHTML = "";
+        const searchedBook = renderReadlist(searchTitle());
+        bookList.append(searchedBook);
     });
 
     completeCheckBox.addEventListener('change', () => {
@@ -179,11 +190,11 @@ const searchTitle = () => {
         const bookTitleArray=  book.title.toLowerCase().split(' ');
         for(const title of titleSplit){
             if (bookTitleArray.includes(title)){
-                return location.href = `#read-${book.id}`;
+                return book;
             }
         }
     }
-    alert('Buku yang dicari tidak ditemukan!');
+    return alert('Buku yang dicari tidak ditemukan!');
 }
 
 const isStorageExist = () => {
